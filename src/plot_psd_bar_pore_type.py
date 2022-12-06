939897dc628f13ee11cc61bc660c863e79143b03
shivamrkparashar <shivamrkparashar@gmail.com>
2022-12-06 14:29:26 -0500

    minor changes

--- src/plot_psd_bar_pore_type.py
+++ src/plot_psd_bar_pore_type.py
@@ -20,10 +20,10 @@ pore_type_histogram = np.array([[0.0000e+00, 0.0000e+00, 0.0000e+00, 0.0000e+00,
 
 def plot_psd_bar_pore_type(bin_centers, pore_type_histogram):
 
-    if config.Npores == 2:
+    if config.pore_type_count == 2:
         d = {'bin_centers': bin_centers, 'pore_type_hist_1': pore_type_histogram[0],
          'pore_type_hist_2':pore_type_histogram[1]}
-    elif config.Npores == 3:
+    elif config.pore_type_count == 3:
         d = {'bin_centers': bin_centers, 'pore_type_hist_1': pore_type_histogram[0],
              'pore_type_hist_2':pore_type_histogram[1], 'pore_type_hist_3': pore_type_histogram[2]}
     df = pd.DataFrame(data =d)
@@ -40,7 +40,7 @@ def plot_psd_bar_pore_type(bin_centers, pore_type_histogram):
     colors = ['orange', 'green', 'skyblue'] # UiO-66
 
     figure()
-    for i in range(config.Npores):
+    for i in range(config.pore_type_count):
         if i == 0:
             bar(bin_centers, pore_type_histogram[i], width = width_bar,
                 label = 'pore %d' %(i+1), color = colors[i], edgecolor = 'black')
@@ -59,5 +59,6 @@ def plot_psd_bar_pore_type(bin_centers, pore_type_histogram):
     ylim(0, )
     ylabel('Normalized Distribution')
     savefig('ptd_psd.jpg' ,format='jpg',dpi=300,bbox_inches='tight')
+
 
 #show()
